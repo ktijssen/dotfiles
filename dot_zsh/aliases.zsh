2c27b266b7e4e47e2ab24830fc6b52fb40860fab
Kevin Tijssen <kevin@Kevins-MacBook-Pro.local>
2024-03-01 11:15:04 +0100

    Update .config/starship.toml Update .zsh/aliases.zsh

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -1,3 +1,22 @@
 ### ALIASES ###
 
 alias k="kubectl"
+
+# Git Folders
+alias github="cd ~/git/github.com"
+alias gitlab="cd ~/git/gitlab-deploy.kevintijssen.eu"
+alias gitazure="cd ~/git/dev.azure.com"
+alias gitfq="cd ~/git/git.fullstaq.com"
+
+# Krew
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+# Random String
+alias randomstring="tr -dc A-Za-z0-9 </dev/urandom | head -c 24; echo"
+
+# Eza
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons --group-directories-first -l"
+
+# VSCode
+alias code="open -a 'Visual Studio Code'"
